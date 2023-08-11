2def82cd48d2d6cdd8038e6e7b8bbf51c239cd23
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-08-10 20:21:07 -0700

    Add shade function and clear button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 
-let sideSquares = 5
+let sideSquares = 25
+
+let totalSquares = sideSquares * sideSquares
+
+//function to create a single row
 
 function createRow () {
   let container = document.getElementById('container')
@@ -14,7 +18,7 @@ function createRow () {
   }
 }
 
-//createRow()
+//function to create the grid
 
 function createGrid () {
   for (let i = 0; i < sideSquares; i++) {
@@ -23,3 +27,27 @@ function createGrid () {
 }
 
 createGrid()
+
+//shade
+
+let square = document.getElementsByClassName('gridSquare')
+
+for (let i = 0; i < totalSquares; i++) {
+  square[i].addEventListener('mouseover', shade)
+  function shade() {
+    square[i].style.backgroundColor = 'red'
+  }
+}
+
+//button1 - grid size selection
+
+//button2 - clear grid
+
+let button1 = document.getElementById('button2');
+button1.addEventListener('click', clear)
+
+function clear() {
+  for (let i = 0; i < totalSquares; i++) {
+    square[i].style.backgroundColor = '#e2e1e1'
+  }
+}
