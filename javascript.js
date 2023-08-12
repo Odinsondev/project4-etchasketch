049625cd56168ad0df6b7d10d08d9dea1d4b5e2d
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2023-08-11 17:34:23 -0700

    Add rgb shade and shade switch functions

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let sideSquares = 25;
 
 let totalSquares = sideSquares * sideSquares;
 
+let currentShade = 1    //variable to reference after changing square count
+
 //function to create a single row
 
 function createRow () {
@@ -33,22 +35,62 @@ createGrid();
 
 let square = document.getElementsByClassName('gridSquare');
 
-function shade() {
+function shade1() {
+
+  currentShade = 1;
+  console.log(currentShade);
+
   let totalSquares = sideSquares * sideSquares; //added as otherwise function will use old value
 
   for (let i = 0; i < totalSquares; i++) {
-    square[i].addEventListener('mouseover', shade);
-    function shade() {
+    square[i].addEventListener('mouseover', shadeColor1);
+    function shadeColor1() {
       square[i].style.backgroundColor = 'CornflowerBlue';
       square[i].style.border = '1px solid rgb(127, 166, 238)';
     }
   }
 }
 
-shade();
+shade1();
 
 //shade square with random RGB
 
+function shade2() {
+
+  currentShade = 2;
+  console.log(currentShade);
+
+  let totalSquares = sideSquares * sideSquares; //added as otherwise function will use old value
+
+  for (let i = 0; i < totalSquares; i++) {
+    square[i].addEventListener('mouseover', shadeColor2);
+    function shadeColor2() {
+
+      let red = Math.floor(Math.random() * 256);
+      let green = Math.floor(Math.random() * 256);
+      let blue = Math.floor(Math.random() * 256);
+      let randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    
+      square[i].style.backgroundColor = randomColor;
+      square[i].style.border = '1px solid rgb(228, 240, 200)';
+    }
+  }
+}
+
+//shade2();
+
+//switch between shade methods
+
+
+function switchShade() {
+  let button3 = document.getElementById('button3');
+  let button4 = document.getElementById('button4');
+  button3.addEventListener('click', shade1);
+  button4.addEventListener('click', shade2);
+
+}
+
+switchShade();
 
 
 //button1 - grid size selection prompt
@@ -63,12 +105,22 @@ function getSize() {
     deleteGrid();
     sideSquares = sideSquares2;
     createGrid();
-    shade();
+
+    if (currentShade ===1) {
+      shade1();
+    } else {
+      shade2();
+    }
+    
   } else {
     deleteGrid();
     sideSquares = sideSquares2;
     createGrid();
-    shade();
+    if (currentShade ===1) {
+      shade1();
+    } else {
+      shade2();
+    }
   }
 }
 
